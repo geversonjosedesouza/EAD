d6930291a2dc97844f2accd74b15fb0180bbf283
geverson <geverson@localhost>
2018-07-22 12:52:00 -0300

    novo commit 22/07

---
+++ src/aula05/UsuarioAdministrador.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula05;
+
+import java.util.Date;
+
+/**
+ *
+ * @author geverson
+ */
+public class UsuarioAdministrador extends UsuarioPadrao {
+
+    public UsuarioAdministrador(int codigo, String nome, String senha, Date dataCadastro) {
+        super(codigo, nome, senha, dataCadastro);
+    }
+
+}
---
+++ src/aula05/UsuarioPadrao.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula05;
+
+import java.util.Date;
+
+/**
+ *
+ * @author geverson
+ */
+public class UsuarioPadrao {
+
+    public int codigo;
+    public String nome;
+    public String senha;
+    public Date dataCadastro;
+
+    public UsuarioPadrao(int codigo, String nome, String senha, Date dataCadastro) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.senha = senha;
+        this.dataCadastro = dataCadastro;
+    }
+
+}
